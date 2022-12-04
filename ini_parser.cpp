d3b4788408288a08cc685cfee63419d3fd955448
Andrei Dogadaev <dogadlev@yandex.ru>
2022-12-04 22:02:35 +0300

    Change ini_parser.cpp and inputs.ini

--- ini_parser.cpp
+++ ini_parser.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
@@ -12,61 +12,30 @@ public:
     /*
     Конструктор в качестве параметра принимает имя файла.
     Открывает файл, читает данные, отбрасывая пустые строки и комментарии - метод readFile()
-    Метод parse() - анализирует "сырые" данные и сохраняет результат в std::map
+    Метод parse() - анализирует "сырые" данные и сохраняет результат в std::map<> data
     */
-    Parser(std::string fileName)
+    Parser(std::string &&fileName)
     {
         readFile(fileName);
         parse();
     }
 
+
     /*
-    Метод get_value() возвращает значение переменной.
-    Используется ключ формата section.var
-    Если ключ не найден, выбрасывается исключение.
-    Отсутствие ключа свидетельствует либо об отсутствии сечения, либо об отсутствии запрашиваемой переменной.
+    Метод get_value() возвращает значение, которое находит приватный метод findValue().
     */
     template <class T>
     T get_value(std::string &&sectionVar)
     {
-        const auto itr = data.find(sectionVar);
-        if (itr == data.end())
-            throw std::runtime_error("Значение не было обнаружено. Проверьте имя секции и имя переменной.");
-        return itr->second;
+        return find_value<T>(sectionVar);
     }
-
-    /* template <>
-    int get_value(std::string &&sectionVar)
-    {
-        const auto itr = data.find(sectionVar);
-        if (itr == data.end())
-            throw std::runtime_error("Value was not found! Check section and variable names.");
-        auto res = stoi(itr->second);
-        return res;
-    }
-
-    template <>
-    double get_value(std::string &&sectionVar)
-    {
-        const auto itr = data.find(sectionVar);
-        if (itr == data.end())
-            throw std::runtime_error("Value was not found! Check section and variable names.");
-        auto res = stod(itr->second);
-        return res;
-    } */
-
-    /* void printData()
-    {
-        for (auto itr = data.begin(); itr != data.end(); ++itr)
-            std::cout << itr->first << " : " << itr->second << std::endl;
-    } */
 
 private:
     std::vector<std::string> rawData{};
     std::map<std::string, std::string> data{};
 
     /*
-    Метод readFile() читает и отбрасывает не нужные данные (пустые строки и комментарии).
+    Метод readFile() читает и отбрасывает не нужные данные (пустые строки, комментарии, излишние пробелы).
     При возникновении проблем с открытием файла, выбрасывается исключение.
     Файл читается построчно.
     Результат сохраняется в вектор - "сырые" данные.
@@ -89,6 +58,12 @@ private:
                 size_t pos = buff.find(";");
                 if (pos != std::string::npos)
                     buff.erase(pos, std::string::npos);
+                pos = buff.find(" ");
+                while (pos != std::string::npos)
+                {
+                    buff.erase(pos, 1);
+                    pos = buff.find(" ");
+                }
                 rawData.push_back(buff);
             }
         }
@@ -96,12 +71,11 @@ private:
     }
 
     /*
-    Метод parse() анализирует вектор rawData и сохраняет результаты в std::map data.
+    Метод parse() анализирует вектор rawData и сохраняет результаты в std::map<> data.
     Если в ячейке вектора обнаружена подстрока Section, обнаруживается номер секции, результат сохраняется в переменную sec.
     Если в ячейке вектора не обнаружена подстрока Section, обнаруживается имя переменной и значение.
     В качестве "маркера" используется =.
-    ВАЖНО!!! На данном этапе парсер работает не корректно, если между именем и/или значением переменной и = содержатся пробелы.
-    После обнаружения имени и значения переменной формируется ключ и данные сохраняются в data. 
+    После обнаружения имени и значения переменной формируется ключ и данные сохраняются в data.
     */
     void parse()
     {
@@ -127,18 +101,50 @@ private:
             }
         }
     }
+
+    /*
+    Метод find_value() возвращает значение переменной.
+    Используется ключ формата section.var
+    Если ключ не найден, выбрасывается исключение.
+    Отсутствие ключа свидетельствует либо об отсутствии сечения, либо об отсутствии запрашиваемой переменной.
+    */
+    template <class T>
+    T find_value(std::string& sectionVar)
+    {
+        const auto itr = data.find(sectionVar);
+        if (itr == data.end())
+            throw std::runtime_error("Значение не было обнаружено. Проверьте имя секции и имя переменной.");
+        return itr->second;
+    }
 };
+
+template <>
+double Parser::get_value(std::string &&sectionVar)
+{
+    auto res = stod(Parser::find_value<std::string>(sectionVar));
+    return res;
+}
+
+template <>
+int Parser::get_value(std::string &&sectionVar)
+{
+    if(sectionVar.find(".") != std::string::npos)
+        throw std::invalid_argument("");
+    auto res = stoi(Parser::find_value<std::string>(sectionVar));
+    return res;
+}
 
 int main(int argc, char **argv)
 {
+
     try
     {
         Parser ini_parser("inputs.ini");
 
-        //ini_parser.printData();
-
-        auto value = ini_parser.get_value<std::string>("Section1.var2");
-        std::cout << value;
+        auto value = ini_parser.get_value<int>("Section1.var1");
+        std::cout << value << std::endl;
+        value += value;
+        std::cout << value << std::endl;
 
         return 0;
     }
